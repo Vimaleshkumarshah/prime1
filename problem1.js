6b9caaf6098ba04b5ba5d7e022dbf6926e615ffa
ranjit barnwal <ranjitmdp2000@gmail.com>
2022-09-13 18:22:52 +0530

    2nd adding code

--- problem1.js
+++ problem1.js
@@ -1,6 +1,14 @@
  
 
- let n=13
+ let n=13;
+ let count=0;
  for(let i=0;i<=N-1;i++){
-    
+    for(let j=1; j<=n; j++){
+        if(i%j==0){
+            count++
+        }
+    }
+ }
+ if(count==2){
+    console.log(prime)
  }
