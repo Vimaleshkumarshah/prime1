e0bf40ab519b157ef7004718d62b21826c54056d
ranjit barnwal <ranjitmdp2000@gmail.com>
2022-09-13 19:50:46 +0530

    adding complete

--- problem1.js
+++ problem1.js
@@ -2,13 +2,14 @@
 
  let n=13;
  let count=0;
- for(let i=0;i<=N-1;i++){
-    for(let j=1; j<=n; j++){
-        if(i%j==0){
-            count++
-        }
+ for(let i=0;i<=n;i++){
+    if(n%i==0){
+        count++
     }
  }
  if(count==2){
-    console.log(prime)
+    console.log("prime")
+ } else{
+    console.log("Not a prime")
  }
+ 
